1254d3da62aba325ff7ce97ebcaae2f64978a9bd
ohtaeg <otk1090@naver.com>
2019-11-24 22:51:31 +0900

    (test) : 투입된 금액이 있을 경우 0원을 출력할 수 없다.

--- vending-machine/src/test/java/VendingMachineTest.java
+++ vending-machine/src/test/java/VendingMachineTest.java
@@ -2,6 +2,8 @@ import org.junit.jupiter.api.BeforeEach;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -19,6 +21,22 @@ class VendingMachineTest {
     public void getReturnAmount_Zero() {
         assertThat(vendingMachine.getCurrentMoney()).isZero();
     }
+
+    @ParameterizedTest
+    @DisplayName("[금액 출력] : 투입된 금액이 있을 경우 0원을 출력할 수 없다.")
+    @ValueSource(ints = 100)
+    public void getCurrentMoney_NotZero(int coin) {
+        // given : 투입된 금액이 존재함.
+        vendingMachine.insertCoin(coin);
+
+        // when :
+        int returnedAmount = vendingMachine.getCurrentMoney();
+
+        // then
+        assertThat(returnedAmount).isNotZero();
+    }
+
+
 
 
 }
